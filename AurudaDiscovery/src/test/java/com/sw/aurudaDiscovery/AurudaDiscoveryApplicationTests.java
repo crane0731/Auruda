ecb8a1a20100343e9c78212ae0296bcdd09c7627
crane0731 <dlwnsgkr8318@naver.com>
2024-11-21 18:10:33 +0900

    Add all subprojects as part of the Auruda monorepo

---
+++ AurudaDiscovery/src/test/java/com/sw/aurudaDiscovery/AurudaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.aurudaDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurudaDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
