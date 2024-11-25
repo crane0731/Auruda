cc04f99cb4be2620ae492f7830b9613586c06569
crane0731 <dlwnsgkr8318@naver.com>
2024-11-25 16:21:58 +0900

    Remove AurudaDiscovery module

--- AurudaLogin/src/main/java/com/sw/AurudaLogin/AurudaLoginApplication.java
+++ AurudaLogin/src/main/java/com/sw/AurudaLogin/AurudaLoginApplication.java
@@ -2,13 +2,12 @@ package com.sw.AurudaLogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableDiscoveryClient
+
 public class AurudaLoginApplication {
 
 	public static void main(String[] args) {
